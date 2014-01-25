18eea26bc1addec4b3e50e80fe2eccbc89636c58
Sergey Sidorchick <whitekrokodile@gmail.com>
2014-01-25 16:04:45 +0300

    Field CollectionView tests added.

--- test/spec/views/collection/field.js
+++ test/spec/views/collection/field.js
@@ -1,25 +1,42 @@
 (function() {
-	'use strict';
+  'use strict';
 
-	var root = this;
+  var root = this;
 
-	root.define([
-		'views/collection/field'
-		],
-		function( Field ) {
+  root.define([
+    'jquery',
+    'views/collection/field',
+    'collections/field'
+    ],
+    function($, Field, Collection) {
 
-			describe('Field Collectionview', function () {
+      describe('Field CollectionView', function () {
 
-				it('should be an instance of Field Collectionview', function () {
-					var field = new Field();
-					field.should.be.an.instanceof(Field);
-				});
+        it('should be an instance of Field CollectionView', function () {
+          var field = new Field();
+          field.should.be.an.instanceof(Field);
+        });
 
-				it('should have more test written', function(){
-					expect( false ).to.be.ok;
-				});
-			});
+        it('should toggle Cell Model alive state by click', function () {
+          var collection = new Collection(null, { height: 10, width: 10 });
+          var field = new Field({ collection: collection });
+          field.render();
 
-		});
+          field._clickCell(13, 12);
+          collection.at(11).get('isAlive').should.be.true;
+
+          field._clickCell(13, 12);
+          collection.at(11).get('isAlive').should.be.false;
+
+          field._clickCell(0, 0);
+          collection.at(0).get('isAlive').should.be.true;
+
+          field._clickCell(100, 100);
+          collection.at(99).get('isAlive').should.be.true;
+        });
+
+      });
+
+    });
 
 }).call( this );
